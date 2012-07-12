85f87d88c229f500d323b4f9112ff179b8e14c65
brad barden <iamb@cpan.org>
2012-07-12 16:34:57 +0000

    Import of IAMB/MooseX-ScopedConfig-0.02 from CPAN.
    gitpan-cpan-distribution: MooseX-ScopedConfig
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         IAMB/MooseX-ScopedConfig-0.02.tar.gz
    gitpan-cpan-author:       IAMB
    gitpan-cpan-maturity:     released

--- lib/MooseX/ScopedConfig.pm
+++ lib/MooseX/ScopedConfig.pm
@@ -11,11 +11,11 @@ MooseX::ScopedConfig - Moose eXtension to use Config::Scoped
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
